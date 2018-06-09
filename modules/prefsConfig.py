c078c2905fbe60117521b8e272582884b1dd2538
Huey Yeng <huey.yeng.mmu@gmail.com>
2018-06-09 23:11:18 +0800

    Change write bytes as str
    - Code migration from Python 2.7 to 3.6

--- modules/prefsConfig.py
+++ modules/prefsConfig.py
@@ -8,10 +8,11 @@ else:
     import configparser
     config = configparser.ConfigParser()
 
-
 ROOT_DIR = ''
 INI_FILE = 'settings.ini'
 INI_PATH = (ROOT_DIR + INI_FILE)
+
+home = os.path.expanduser('~')  # Defaults to home directory
 
 
 def create_config(INI_PATH):
@@ -19,18 +20,16 @@ def create_config(INI_PATH):
     config.optionxform = str
 
     config.add_section('Settings')
-    home = os.path.expanduser('~')  # Defaults to home directory
-
     config.set('Settings', 'ProjectPath', home.replace('\\', '/'))
     config.set('Settings', 'ShowDescriptionPanel', 'True')
     config.set('Settings', 'CurrentProject', '')
 
     config.add_section('UI')
-
     config.set('UI', 'Font', 'Arial')
     config.set('UI', 'Theme', 'Fusion')
 
-    with open(INI_PATH, 'wb') as config_file:
+    # with open(INI_PATH, 'wb') as config_file:
+    with open(INI_PATH, 'w') as config_file:
         config.write(config_file)
 
 
@@ -38,10 +37,10 @@ def get_config(INI_PATH):
     # Returns the config object
     if not os.path.exists(INI_PATH):
         create_config(INI_PATH)
-        print ('ERROR INI FILE NOT FOUND')
-        print ('Creating INI file at ' + INI_PATH)
+        print('ERROR INI FILE NOT FOUND')
+        print('Creating INI file at ' + INI_PATH)
 
-    config.optionxform=str
+    config.optionxform = str
     config.read(INI_PATH)
     return config
 
@@ -50,8 +49,8 @@ def get_setting(INI_PATH, section, setting):
     # Print out a setting
     config = get_config(INI_PATH)
     value = config.get(section, setting)
-    # print '{section} {setting} is {value}'.format(
-    #     section=section, setting=setting, value=value)
+    # print('{section} {setting} is {value}'.format(
+    #     section=section, setting=setting, value=value))
     return value
 
 
@@ -59,7 +58,7 @@ def update_setting(INI_PATH, section, setting, value):
     # Update a setting
     config = get_config(INI_PATH)
     config.set(section, setting, value)
-    with open(INI_PATH, 'wb') as config_file:
+    with open(INI_PATH, 'w') as config_file:
         config.write(config_file)
 
 
@@ -67,7 +66,7 @@ def delete_setting(INI_PATH, section, setting):
     # Delete a setting
     config = get_config(INI_PATH)
     config.set(section ,setting)
-    with open(INI_PATH, 'wb') as config_file:
+    with open(INI_PATH, 'w') as config_file:
         config.write(config_file)
 
 
@@ -84,11 +83,3 @@ CURRENTPROJECT = get_setting(INI_PATH, 'Settings', 'CurrentProject')
 THEME = get_setting(INI_PATH, 'UI', 'Theme')
 
 # -------------------------------------------------------------------
-
-
-# Uncomment the below to run this script manually to generate the INI
-# if __name__ == '__main__':
-#     get_setting(INI_PATH, 'Settings', 'ProjectPath')
-#     get_setting(INI_PATH, 'UI', 'Theme')
-#     create_config(INI_PATH)
-#     update_setting(INI_PATH, 'UI', 'Theme', 'Fusion')
