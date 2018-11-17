d948a8d18997dfcebcb02e7a2524ee62f0a26612
Huey Yeng <huey.yeng.mmu@gmail.com>
2018-11-18 01:09:47 +0800

    Fix 'test_create_config' unit test

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -1,12 +1,25 @@
 """Test Create Config"""
+import configparser
 from config.configurations import create_config
 
-ini_content = open('tests/samples/sample.ini', 'r')
+config = configparser.ConfigParser()
 
 
 def test_create_config_successful(tmpdir):
     ini_file = tmpdir.join('test.ini')
     create_config(ini_file)
-    created_ini = ini_file.read()
-    existing_ini = ini_content.read()
-    assert created_ini == existing_ini
+    config.read(ini_file)
+
+    expected_sections_options = [
+        ('Settings', 'ProjectPath'),
+        ('Settings', 'ShowDescriptionPanel'),
+        ('Settings', 'CurrentProject'),
+        ('UI', 'Font'),
+        ('UI', 'Theme'),
+        ('Assets', 'CategoryList'),
+        ('Assets', 'SubfolderList'),
+    ]
+
+    for section, option in expected_sections_options:
+        print(section, option)
+        assert config.has_option(section, option) is True
