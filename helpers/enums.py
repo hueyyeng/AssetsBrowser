297f4ba0642583f9706f90094795991a333e3843
Huey Yeng <huey.yeng.mmu@gmail.com>
2020-03-22 03:01:15 +0800

    AB-7: Update prosector config
    - Increase max attributes to 20
    - Ignore venv directory

--- helpers/enums.py
+++ helpers/enums.py
@@ -1,11 +1,14 @@
 """Helpers Constants"""
+from enum import Enum
 
-SELECTED_FILE = {
-    'File': '',
-    'Path': '',
-}
 FILE_MANAGER = {
     'Windows': 'Explorer',
     'Darwin': 'Finder',
     'Linux': 'File Manager',
 }
+
+
+class FileManager(Enum):
+    WINDOWS = 'Explorer'
+    DARWIN = 'Finder'
+    LINUX = 'File Manager'
